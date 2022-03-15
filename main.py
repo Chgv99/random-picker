4e98694a5613f029151bcdf7bc8e3c93182ef4e9
chgv99 <christiangv99@gmail.com>
2022-03-15 20:00:32 +0000

    Image code

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import os
+#from PIL import Image
+import random
+#from datetime import datetime
+import time
+
+#class FileType(Enum):
+#    IMAGE = 1
+#    TEST = 2
+
+# Params
+#keyword = sys.argv[1]
+path = sys.argv[1]
+extensions = []
+
+# Extensions
+#image = [ ".png", ".jpg", ".png", ".png", ]
+
+if not os.path.isdir(path):
+    raise ValueError("Not a directory.")
+
+# Check for file type
+#if keyword == FileType.IMAGE:
+#    extensions.append(".png");
+
+#files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+files = []
+for (dirpath, dirnames, filenames) in os.walk(path):
+    files.extend(filenames)
+
+random.seed(time.time())
+file = files.pop(random.randrange(0, len(files)))
+print("Selected file: " + file)
+
+#im = Image.open(os.path.join(path, f))
+#im.show()
+os.startfile(os.path.join(path, file))
